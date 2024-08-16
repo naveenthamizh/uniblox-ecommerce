6b7c8cd744c532c1ea5b6986bcd985dba50626e9
naveenthamizh <naveenkumar.ksrct@gmail.com>
2024-08-16 23:20:18 +0530

    Added alert for prerequisites

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { useLayoutEffect } from "react";
+
 import Content from "./Components/Content";
 import Header from "./Components/Header";
 
 import "./App.css";
 
 function App() {
+  // Prerequisites alert
+  useLayoutEffect(() => {
+    alert("I hope you have gone through prerequisites in Readme file ");
+  }, []);
   return (
     <main className="App">
       <Header />
